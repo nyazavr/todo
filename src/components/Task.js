a514dda06ab251158e5813f9059a4994a2826643
nyazavr <nazarov-zhora@mail.ru>
2023-10-02 18:54:12 +0500

    ---

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,106 +1,87 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import classNames from 'classnames';
 
-export default class Task extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      min: '',
-      sec: '',
-      countTime: null,
-      pauseChec: false,
-      leftTime: 0,
-      editing: false,
-      label: '',
-    };
-    this.textInput = React.createRef();
-  }
+function Task(props) {
+  const [min, setMin] = useState(props.minut);
+  const [sec, setSec] = useState(props.second);
+  const [countTime, setCountTime] = useState(Number(props.second) + Number(props.minut * 60));
+  const [pauseChec, setPauseChec] = useState(0);
+  const [editing, setEditing] = useState(false);
+  const [label, setLabel] = useState('');
+  const textInput = React.createRef();
 
-  componentDidMount = () => {
-    this.setState({
-      min: this.props.minut,
-      sec: this.props.second,
-      countTime: Number(this.props.second) + Number(this.props.minut * 60),
-    });
-  };
-  componentDidUpdate = () => {
-    if (this.textInput.current) {
-      this.textInput.current.focus();
+  useEffect(() => {
+    if (textInput.current) {
+      textInput.current.focus();
     }
-    let newCountTime = this.state.countTime - 1;
-    if (this.state.pauseChec && newCountTime) {
+    let newCountTime = countTime - 1;
+    if (pauseChec && newCountTime) {
       setTimeout(() => {
-        this.setState({ min: parseInt(newCountTime / 60), sec: newCountTime % 60, countTime: newCountTime });
+        setMin(parseInt(newCountTime / 60));
+        setSec(newCountTime % 60);
+        setCountTime(newCountTime);
       }, 1000);
     }
+  });
+
+  const onLabelChange = (e) => {
+    setLabel(e.target.value);
   };
 
-  onLabelChange = (e) => {
-    this.setState({
-      label: e.target.value,
-    });
-  };
-
-  onSubmit = (e) => {
+  const onSubmit = (e) => {
     e.preventDefault();
-    this.state.label.trim() ? this.props.onItemEdit(this.state.label, this.props.id) : null;
-    this.setState({
-      editing: false,
-      label: '',
-    });
+    label.trim() ? props.onItemEdit(label, props.id) : null;
+    setEditing(false);
+    setLabel('');
   };
 
-  onClickEdit = () => {
-    this.setState({ editing: !this.state.editing });
+  const onClickEdit = () => {
+    setEditing(!editing);
   };
-  handleKeyDown = (event) => {
+
+  const handleKeyDown = (event) => {
     console.log('User pressed: ', event.key);
-    event.key == 'Escape' ? this.setState({ editing: false }) : null;
+    event.key == 'Escape' ? setEditing(false) : null;
   };
 
-  render() {
-    const { description, createdDate, id, onDeleted, onClickLabel, completed, visible } = this.props;
-    console.log(this.state.countTime);
-    return visible ? (
-      <li
-        className={classNames({ completed: completed ? true : false }, { editing: this.state.editing ? true : false })}
-        key={id}
-      >
-        <div className="view">
-          <input className="toggle" type="checkbox" defaultChecked={completed} onClick={onClickLabel} id={'id:' + id} />
-          <label htmlFor={'id:' + id}>
-            <span className="title">{description}</span>
-            <span className="description">
-              <button className="icon icon-play" onClick={() => this.setState({ pauseChec: true })}></button>
-              <button className="icon icon-pause" onClick={() => this.setState({ pauseChec: false })}></button>
-              {this.state.min}:{this.state.sec}
-            </span>
-            <span className="created description">{createdDate}</span>
-          </label>
-          <button className="icon icon-edit" onClick={this.onClickEdit}></button>
-          <button className="icon icon-destroy" onClick={onDeleted}></button>
-        </div>
-        {this.state.editing ? (
-          <form onSubmit={this.onSubmit}>
-            <input
-              tabIndex={0}
-              onKeyDown={this.handleKeyDown}
-              onBlur={() => {
-                this.setState({
-                  editing: false,
-                });
-              }}
-              onChange={this.onLabelChange}
-              type="text"
-              ref={this.textInput}
-              className="edit"
-              value={this.state.label}
-            />
-          </form>
-        ) : (
-          ''
-        )}
-      </li>
-    ) : null;
-  }
+  const { description, createdDate, id, onDeleted, onClickLabel, completed, visible } = props;
+
+  return visible ? (
+    <li className={classNames({ completed: completed ? true : false }, { editing: editing ? true : false })} key={id}>
+      <div className="view">
+        <input className="toggle" type="checkbox" defaultChecked={completed} onClick={onClickLabel} id={'id:' + id} />
+        <label htmlFor={'id:' + id}>
+          <span className="title">{description}</span>
+          <span className="description">
+            <button className="icon icon-play" onClick={() => setPauseChec(true)}></button>
+            <button className="icon icon-pause" onClick={() => setPauseChec(false)}></button>
+            {min}:{sec}
+          </span>
+          <span className="created description">{createdDate}</span>
+        </label>
+        <button className="icon icon-edit" onClick={onClickEdit}></button>
+        <button className="icon icon-destroy" onClick={onDeleted}></button>
+      </div>
+      {editing ? (
+        <form onSubmit={onSubmit}>
+          <input
+            tabIndex={0}
+            onKeyDown={handleKeyDown}
+            onBlur={() => {
+              setEditing(false);
+            }}
+            onChange={onLabelChange}
+            type="text"
+            ref={textInput}
+            className="edit"
+            value={label}
+          />
+        </form>
+      ) : (
+        ''
+      )}
+    </li>
+  ) : null;
 }
+
+export default Task;
